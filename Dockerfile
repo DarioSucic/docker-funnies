614ce7f40380e3178887e65e0f6b0bf5605fa0d3
Dario Sucic <dariosucic@gmail.com>
2022-06-03 14:14:13 +0200

    rmrmrm

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 FROM rust:1.59.0 as builder
 WORKDIR /build
 COPY code/ /build
-RUN rustup target add x86_64-unknown-linux-musl
-RUN RUSTFLAGS="-C target-feature=+crt-static" cargo build --release --target x86_64-unknown-linux-musl
-RUN strip -s /build/target/x86_64-unknown-linux-musl/release/docker_playground
+RUN rustup target add armv7-unknown-linux-musleabihf
+RUN RUSTFLAGS="-C target-feature=+crt-static" cargo build --release --target armv7-unknown-linux-musleabihf
+RUN strip -s /build/target/armv7-unknown-linux-musleabihf/release/docker_playground
 
 FROM gcr.io/distroless/static
 WORKDIR /app
-COPY --from=builder /build/target/x86_64-unknown-linux-musl/release/docker_playground /app
+COPY --from=builder /build/target/armv7-unknown-linux-musleabihf/release/docker_playground /app
 ENTRYPOINT ["/app/docker_playground"]
--- code/src/main.rs
+++ code/src/main.rs
@@ -1,3 +1,52 @@
+use std::{time::Instant};
+
+use serde::Deserialize;
+
+pub fn black_box<T>(dummy: T) -> T {
+    unsafe {
+        let ret = std::ptr::read_volatile(&dummy);
+        std::mem::forget(dummy);
+        ret
+    }
+}
+#[derive(Deserialize)]
+struct FileMetadata {
+    id: String,
+    name: String,
+    thumbnail: Option<String>,
+    sourceFileType: String,
+    providedFileType: String,
+
+    // deprecated in v2
+    fileType: String,
+}
+
+#[inline(never)]
+fn decode(data: &[u8]) -> FileMetadata {
+    serde_json::from_slice(data).expect("Deserialization error")
+}
+
+fn benchmark(name: &str, data: &[u8], n: u32) {
+    let mut filemeta: FileMetadata;
+    
+    let st = Instant::now();
+
+    for _ in 0..n {
+        filemeta = decode(data);
+        black_box(filemeta);
+    }
+    
+    let dt = st.elapsed();
+    
+    println!("{name:24} :: {:>12?} ({n} iterations)", dt);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let metadata_bytes: &[u8] = include_bytes!("../metadata.json");
+    let metadata_w_thumbnail_bytes: &[u8] = include_bytes!("../metadata_w_thumbnail.json");
+
+    let n = 128;
+
+    benchmark("metadata", metadata_bytes, n);
+    benchmark("metadata /w thumbnail", metadata_w_thumbnail_bytes, n);
 }
